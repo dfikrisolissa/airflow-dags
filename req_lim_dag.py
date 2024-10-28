094fe2990b7cc8dafa726a3b8edd1f8c6f54a325
dfikrisolissa <suyeti.astin58@gmail.com>
2024-10-28 07:21:30 +0700

    Create req_lim_dag.py

---
+++ req_lim_dag.py
@@ -0,0 +1,37 @@
+from airflow.decorators import dag, task
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from datetime import datetime
+
+@dag(start_date=datetime(2023, 1, 1), schedule='@daily', catchup=False)
+def parallel_dag():
+
+    # Task list using KubernetesPodOperator with resource requests and limits
+    tasks = [
+        KubernetesPodOperator(
+            task_id=f'task_{t}',
+            name=f'pod_task_{t}',
+            namespace='default',
+            image='alpine:3.14',  # Use a simple lightweight image for demo
+            cmds=["sh", "-c", "sleep 60"],
+            resources={
+                'request_memory': '256Mi',
+                'request_cpu': '250m',
+                'limit_memory': '512Mi',
+                'limit_cpu': '500m'
+            }
+        ) for t in range(1, 4)
+    ]
+
+    @task
+    def task_4(data):
+        print(data)
+        return 'done'
+    
+    @task
+    def task_5(data):
+        print(data)
+
+    tasks >> task_5(task_4(42))
+
+parallel_dag = parallel_dag()
+
